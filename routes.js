e025d0d9a431c5bbc2e082dbd311fa306f1f2e4c
Albert Lyubarsky <albert.l@comtecglobal.com>
2019-06-25 12:37:04 +0300

    Changes for deployment

--- routes.js
+++ routes.js
@@ -24,6 +24,7 @@ module.exports = function(app) {
     res.end();
   });
   
+
   app.get('/app/*', function(req, res) {
     res.sendStatus(404);
   });
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const path = require('path');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var  port = process.env.PORT || 5000
 
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 require('./expressConfig')(app);
 
