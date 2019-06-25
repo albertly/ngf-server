b11f802d0124972509df7f57537ce2d46d327045
Albert Lyubarsky <albert.l@comtecglobal.com>
2019-06-25 17:21:39 +0300

    Add cors

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const path = require('path');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var  port = process.env.PORT || 5000
 
+var cors = require('cors');
 var app = express();
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 require('./expressConfig')(app);
