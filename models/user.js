336400f4d496a14638ef0bba673688393838dc72
albertly <albertly@yandex.ru>
2019-08-10 23:30:50 +0300

    Google Auth - change userSchema

--- models/user.js
+++ models/user.js
@@ -4,16 +4,23 @@ const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
-  email: { type: String, unique: true, lowercase: true, trim: true, match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/},
+  email: { type: String, unique: true, lowercase: true, trim: true, match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ },
   password: String,
   userName: String,
   firstName: String,
   lastName: String,
+  googleProvider: {
+    type: {
+      id: String,
+      token: String
+    },
+    select: false
+  }
 });
 
 
-userSchema.methods.comparePassword = function(candidatePassword, callback) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
     if (err) { return callback(err); }
 
     callback(null, isMatch);
