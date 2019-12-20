7ac595934506c40b7a02b7145d19f6bbf5f4f451
albertly <albertly@yandex.ru>
2019-12-20 22:31:44 +0200

    Cashe error handling

--- services/cache.js
+++ services/cache.js
@@ -3,14 +3,37 @@ const redis = require('redis');
 const util = require('util');
 const keys = require('../config/keys');
 
-const client = redis.createClient({host:keys.redis.host, port:keys.redis.port, password:keys.redis.auth});
+const client = redis.createClient({host:keys.redis.host, port:keys.redis.port, password:keys.redis.auth,
+  retry_strategy: function (options) {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+        // End reconnecting on a specific error and flush all commands with
+        // a individual error
+        return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+        // End reconnecting after a specific timeout and flush all commands
+        // with a individual error
+        return new Error('Retry time exhausted');
+    }
+    if (options.attempt > 5) {
+        // End reconnecting with built in error
+        return undefined;
+    }
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
+}
+});
+
+client.on("error", function (err) {
+  console.log("Chache Error " + err);
+});
+
 client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function(options = {}) {
-  this.useCache = true;
+  this.useCache = true && client.connected;
   this.hashKey = JSON.stringify(options.key || '');
-
   return this;
 };
 
